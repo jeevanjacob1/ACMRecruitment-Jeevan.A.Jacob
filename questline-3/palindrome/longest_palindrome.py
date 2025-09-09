7ee86002e83d51fe1b7355302deb772e2ecbbed2
jeevanjacob1 <jeevanjacob218@gmail.com>
2025-09-09 09:05:15 +0530

    Add solution for longest palindromic substring
    Add solution for longest palindromic substring

---
+++ questline-3/palindrome/longest_palindrome.py
@@ -0,0 +1,28 @@
+def longest_palindromic(s: str) -> str:
+    if not s:
+        return ""
+    
+    start = 0
+    end = 0
+
+    for i in range(len(s)):
+        len1 = expand_around_center(s, i, i)        #odd len
+        len2 = expand_around_center(s, i, i + 1)  # Even-len
+        max_len = max(len1, len2)
+
+        if max_len > (end - start):
+            start = i - (max_len - 1) // 2
+            end = i + max_len // 2
+
+    return s[start:end + 1]
+
+def expand_around_center(s: str, left: int, right: int) -> int:
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1
+
+# Example usage
+if __name__ == "__main__":
+    input_str = "babad"
+    print("Longest Palindromic Substring:", longest_palindromic(input_str))
